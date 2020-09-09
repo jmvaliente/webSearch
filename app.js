ba741427e44b18ac93874b8c2b1b2c5f4534c41b
Jose Miguel Valiente Niño <valiente_jose2@hotmail.com>
2020-09-09 11:19:36 +0200

    add express, cors, dotenv. Config Server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server Running in port: ${PORT}`);
+});
